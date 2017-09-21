c1c174703b1f5a40cbfdaf88b48447611adfa0c5
email@exampal.com <ulugbek_nasyrov@mail.ru>
2017-09-21 20:01:54 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var x = 10;
+var y = 20;
+
+if (x < y) {
+    console.log("X menshe chem Y");
+} else {
+    console.log("X bolshy chem Y");
+}
+ var x = 10;
+var y = 20;
+
+if (x < y) {
+    alert("X menshe chem Y");
+} else {
+    alert("X bolshy chem Y");
+}
+var years = prompt('Сколько вам лет?', 20);
+alert('Вам ' + years + ' лет!')
